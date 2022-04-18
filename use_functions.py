410fa0c406e83b31ada5f425a36d011c4390e46b
dmitry <ikosha-tam@yandex.ru>
2022-04-18 14:39:43 +0300

    Final

--- use_functions.py
+++ use_functions.py
@@ -34,19 +34,35 @@
 Для реализации основного меню можно использовать пример ниже или написать свой
 """
 
+budget = 0
+history = []
+
+def purchase(budget):
+    sum = int(input('Введите стоимость покупки: '))
+    if sum > budget:
+        print('Недостаточно средств')
+    else:
+        budget -= sum
+        name = input('Введите название покупки: ')
+        history.append((name, sum))
+    return budget
+
 while True:
     print('1. пополнение счета')
     print('2. покупка')
     print('3. история покупок')
     print('4. выход')
-
-    choice = input('Выберите пункт меню')
+    print(f'Ваш счет:{budget}')
+    choice = input('Выберите пункт меню: ')
     if choice == '1':
-        pass
+        sum = int(input('Введите сумму для пополнения: '))
+        budget += sum
     elif choice == '2':
-        pass
+        budget = purchase(budget)
     elif choice == '3':
-        pass
+        for item, cost in history:
+            print(f'{item}:\t {cost} руб.')
+        # print(history)
     elif choice == '4':
         break
     else:
